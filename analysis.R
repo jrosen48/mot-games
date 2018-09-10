3d6f4e3cd346276dc1e38802c58ed9c9ee83b9b0
Joshua Rosenberg <jrosen48@github.com>
2018-09-10 14:42:32 -0400

    update analysis file

--- analysis.R
+++ analysis.R
@@ -36,5 +36,8 @@ m1 <- glmer(persist ~ c_black + c_hisp + c_otherrace + c_male + c_gr4 +
               c_ell_binary + c_frl_binary + 
               c_mathse1 + c_mathut1 + c_mathimp1 + c_intmath1 + stud_c_levelscore +
               ave_c_faillevelscore + c_fsamathscalescore + factor(objective_index)
-            + (1|student_entity_id), family = "binomial", control=glmerControl(optimizer="bobyqa",optCtrl=list(maxfun=2e5)), data = teya_analysis_file_for_r)
+            + (1|student_entity_id), family = "binomial", 
+            control=glmerControl(optimizer="bobyqa",optCtrl=list(maxfun=2e5)), 
+            data = teya_analysis_file_for_r)
+
 summary(m1)
