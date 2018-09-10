ce7eea542a1f6dbf2b18c17ad943f5a724bde797
Joshua Rosenberg <jrosen48@github.com>
2018-09-10 14:42:25 -0400

    update analysis file

--- analysis.R
+++ analysis.R
@@ -2,10 +2,14 @@
 
 library(lme4)
 library(tidyverse)
+# install.packages("here")
+library(here)
+# library(readr) # loaded by tidyverse already
+# install.packages("sjstats")
+library(sjstats)
 
-library(readr)
-teya_analysis_file_for_r <- read_csv("C:/Users/Teya/Google Drive/DAST/DAST DATA--restricted access/2. MIND Data/3. Level Attempts/Pinellas/2017/teya_analysis_file_for_r.csv")
-View(teya_analysis_file_for_r)
+teya_analysis_file_for_r <- read_csv(here("teya_analysis_file_for_r.csv")) # here constructs file paths in a cool way & makes it easy to work cross-platforms
+# View(teya_analysis_file_for_r)
 
 ##test with a simpler model
 mbasic <- glm(persist ~ c_black + c_hisp + c_otherrace + c_male + c_gr4 +
@@ -14,6 +18,17 @@ mbasic <- glm(persist ~ c_black + c_hisp + c_otherrace + c_male + c_gr4 +
                 ave_c_faillevelscore + c_fsamathscalescore + 
                 factor(objective_index), family = "binomial", data = teya_analysis_file_for_r)
 summary(mbasic)
+
+## null model for ICC
+
+m0 <- glmer(persist ~ 1 +
+            (1|student_entity_id) +
+            (1|teacher_entity_id),
+            family = "binomial", 
+            control = glmerControl(optimizer="bobyqa", optCtrl=list(maxfun=2e5)),
+            data = teya_analysis_file_for_r)
+
+icc(m0) # from sjstats package
 
 ##m1 <- glmer(persist ~ c_black + c_hisp + c_otherrace + (1|student_entity_id), family = "binomial", data = teya_analysis_file_for_r)
 
